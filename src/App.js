3415719184aaadbc9d1fa4b10d208548fb03c911
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-04-24 20:43:00 -0500

    NewTask component now creates and POST new task

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import Task from "./app/features/task/Task";
 import Trello from "./app/features/trello/Trello";
 import NewTaskPage from "./app/pages/NewTaskPage/NewTaskPage";
+import CalendarPage from './app/pages/CalendarPage/CalendarPage';
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
             <Route path="/" element={ <Task /> }></Route>
             <Route path="/trello" element={ <Trello /> }></Route>
             <Route path="/new" element={ <NewTaskPage /> }></Route>
+            <Route path="/calendar" element={ <CalendarPage /> }></Route>
         </Routes>
     );
 };
---
+++ src/app/pages/CalendarPage/CalendarPage.js
@@ -0,0 +1,13 @@
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+export default function CalendarPage() {
+
+    return (
+        <div>
+            <h1>This is the Calendar Page</h1>
+        </div>
+    )
+};
